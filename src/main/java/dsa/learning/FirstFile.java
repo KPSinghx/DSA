c3a5f162d57a24b00ddf7cfa3c8208545ee03d02
Kaushlendra Pratap Singh <kaushlendra.singh1@ibm.com>
2025-05-12 18:28:59 +0530

    created git project

---
+++ src/main/java/dsa/learning/FirstFile.java
@@ -0,0 +1,14 @@
+package dsa.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+public class FirstFile {
+
+    public static void main(String[] args) {
+
+        System.out.println("Jai Shiv Shanker");
+    }
+
+}
