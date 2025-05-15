ef812b10fbdfb6733862f59b368341a12fd313dd
Kaushlendra Pratap Singh <kaushlendra.singh1@ibm.com>
2025-05-15 12:46:06 +0530

    added more practice code in LinkedList.java

---
+++ src/main/java/dsa/learning/linkedlist/LinkedListMain.java
@@ -0,0 +1,9 @@
+package dsa.learning.linkedlist;
+
+public class LinkedListMain {
+    public static void main(String args[]){
+LinkedList linkedList=new LinkedList(1);
+linkedList.append(2);
+linkedList.printList();
+    }
+}
